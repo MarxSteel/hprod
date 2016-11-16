7c9830821e0720e432206125954f5e4fb814d765
Marx Medeiros <analista.marx@gmail.com>
2016-11-16 13:59:25 -0200

    Revisado

--- init.php
+++ init.php
@@ -1,59 +1,181 @@
 <?php
-
-// constantes com as credenciais de acesso ao banco MySQL
 $host = "localhost:8889";
 $user = "root";
 $pass = "root";
-$banco = "henrySuporte";
+$banco = "hprod";
 $versao = "3.3.5";
+
+
+// constantes com as credenciais de acesso ao banco MySQL
 define('DB_HOST', $host);
 define('DB_USER', $user);
 define('DB_PASS', $pass);
 define('DB_NAME', $banco);
-
-
-
-
-
-date_default_timezone_set('America/Sao_Paulo'); //DEFININDO O TIMEZONE PARA TODAS AS PÁGINAS
-
-
-$conn = mysqli_connect($host, $user, $pass, $banco) or die("Connection failed: " . mysqli_connect_error());
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+function dbcon()
+{
+    @mysql_connect($host, $user, $pass) or die(mysql_error());
+    @mysql_select_db($banco) or die(mysql_error());
 }
+$IPImpressora = "192.168.60.38";
 
-
-
-//FUNÇÃO PORCENTAGEM_NNX
+//AQUI EU DECLARO A FUNÇÃO DE CHAMAR PORCENTAGEM
+// Y É X% DE Z
 function porcentagem_nnx ($parcial, $porcentagem ) {
  return ($parcial / $porcentagem) * 100;
 }
+    function gerar($sString) {
+  if(!empty($sString)) {
+    $sByteInicial = "02 ";
+    $sTamanhoMensagem = gerarTamanhoString($sString);
+    $sMensagem = stringParaHex($sString);
+    $sCheckSun = gerarCheckSum($sString);
+    $sByteFinal = " 03";
 
+    return $sByteInicial . $sTamanhoMensagem . $sMensagem . $sCheckSun . $sByteFinal;
+  } else
+    return false;
+}
+
+function gerarTamanhoString($sString) {
+  $nTamanhoString = strlen($sString);
+  $nHex1 = $nTamanhoString % 256;
+  $nHex16 = (int) ($nTamanhoString / 256);
+
+  $nHex1 = dechex($nHex1);
+  if(strlen($nHex1) === 1)
+    $nHex1 = "0".$nHex1;
+
+  $nHex16 = dechex($nHex16);
+  if(strlen($nHex16) === 1)
+    $nHex16 = "0".$nHex16;
+
+  $sResultado = $nHex1." ".$nHex16;
+
+  return strtoupper($sResultado);
+}
+
+function stringParaHex($sString) {
+  $sHex = "";
+  $vString = str_split($sString); // Transforma a string em um Array
+  foreach($vString as $sCharactere)  // Percorre cada charactere da string que agora é um Array
+    $sHex .= " ".dechex(ord($sCharactere)); // Transforma esse charactere em ASCII e depois o converte para hexadecimal
+
+    return strtoupper($sHex);  // Converte tudo para letras maiúsculas
+}
+
+function gerarCheckSum($sString) {
+  $nTamanhoString = strlen($sString);
+
+  $sXor = "";
+  $vString = str_split($sString);
+  foreach($vString as $sCharactere)
+    $sXor ^= ord($sCharactere);
+
+  $sXor ^= $nTamanhoString % 256;
+  $sXor ^= $nTamanhoString / 256;
+
+  $nHex1 = $sXor % 16;
+  $nHex16 = (int) ($sXor / 16);
+
+  $sResultado = " ".dechex($nHex16) . dechex($nHex1);
+
+  return strtoupper($sResultado);
+}
+
+function hex2str($hex){
+    $str='';
+    for ($i=0; $i < strlen($hex)-1; $i+=2){
+    $str .= chr(hexdec(substr($hex,$i,2)));
+    }
+    return $str;
+}
+
+function String2Hex($string){
+    $hex='';
+    for ($i=0; $i < strlen($string); $i++){
+        $hex .= dechex(ord($string[$i]));
+    }
+    return $hex;
+}
 
 // habilita todas as exibições de erros
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
-
+date_default_timezone_set('America/Sao_Paulo');
 
 $cor = "skin-blue";
 $Titulo = "Henry Controle de Estoque";
+// MENSAGENS DE PROTOCOLO
+ //ERRO AO CRIAR SOCKET
+ $M000 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          NÃO FOI POSSÍVEL CRIAR SOCKET</div>';
+ // ERRO AO ENVIAR EMPREGADOR
+ $M001 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          Não foi possível Empregador, o relógio não está na rede ou não responde.</div>';
+  // ERRO AO ENVIAR INFORMAÇÃO AO DISPOSITIVO
+  $M002 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          Não foi possível enviar, o relógio não está na rede ou não responde.</div>';
+
+  //ERRO AO ENVIAR DATA E HORA
+  $M003 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          Não foi possível Data/ Hora, o relógio não está na rede ou não responde.</div>';
+
+ //ERRO AO ENVIAR DATA E HORA
+  $M004 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          Não foi possível Data/ Hora, o relógio não está na rede ou não responde.</div>';
+
+  $M005 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          Não foi possível cadastrar o equipamento.</div>';
+
+  //ERRO AO CONECTAR COM O EQUIPAMENTO
+  $M006 = ' <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h4><i class="icon fa fa-alert"></i> Atenção!</h4>
+          Não foi possível comunicar com o equipamento.</div>';
+
+  $SemPrivilegio = '
+    <div class="col-md-12 col-sm-6 col-xs-12">
+     <div class="info-box">
+      <a data-toggle="modal" data-target="#myModal"">
+       <span class="info-box-icon bg-red">
+        <i class="fa fa-exclamation-triangle"></i>
+       </span>
+      </a>
+      <div class="info-box-content">
+       <h4><strong><i>Atenção!</i></strong></h4>
+       <h4>Você não possui privilégios suficientes para abrir esta página. Contate o Administrador!</h4>
+      </div>
+     </div>
+    </div>';
+
+    $NAtAtend = '
+    <div class="col-md-12 col-sm-6 col-xs-12">
+     <div class="info-box">
+      <a data-toggle="modal" data-target="#myModal"">
+       <span class="info-box-icon bg-red">
+        <i class="fa fa-exclamation-triangle"></i>
+       </span>
+      </a>
+      <div class="info-box-content">
+       <h4><strong><i>Atenção!</i></strong></h4>
+       <h4>Você não pode editar esse atendimento pois ja foi finalizado!</h4>
+      </div>
+     </div>
+    </div>';
+
 
 
 require_once 'functions.php';
-
-function TiraCaractere($string) {
-
-    // matriz de entrada
-    $what = array( 'ä','ã','à','á','â','ê','ë','è','é','ï','ì','í','ö','õ','ò','ó','ô','ü','ù','ú','û','À','Á','É','Í','Ó','Ú','ñ','Ñ','ç','Ç',' ','-','(',')',',',';',':','|','!','"','#','$','%','&','/','=','?','~','^','>','<','ª','º' );
-
-    // matriz de saída
-    $by   = array( 'a','a','a','a','a','e','e','e','e','i','i','i','o','o','o','o','o','u','u','u','u','A','A','E','I','O','U','n','n','c','C','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_' );
-
-    // devolver a string
-    return str_replace($what, $by, $string);
-}
