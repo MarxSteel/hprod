f55a702e2032e30340fd1f89f409bd08621fafb3
Marx Medeiros <analista.marx@gmail.com>
2016-11-21 17:22:23 -0200

    Revisado

--- P2/Pendentes.php
+++ P2/Pendentes.php
@@ -17,6 +17,8 @@ require '../QueryUser.php';
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+
 
 </head>
 <body class="hold-transition skin-blue-light fixed sidebar-mini">
