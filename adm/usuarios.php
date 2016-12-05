b17cfbe24ddf6bd897edd968fba7aa8f5f1d756a
Marx Medeiros <analista.marx@gmail.com>
2016-12-05 13:43:53 -0200

    Revisão

--- adm/usuarios.php
+++ adm/usuarios.php
@@ -16,15 +16,12 @@ $QryUser->execute();
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <title><?php echo $titulo; ?></title>
  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
- <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
- <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
- <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
- <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
- <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
-
-
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
 </head>
 <body class="hold-transition skin-blue-light fixed sidebar-mini">
 <div class="wrapper">
@@ -158,39 +155,26 @@ $QryUser->execute();
      </div>
     </div>
     <?php } ?>
-
-
-
-
-
   </div>
  </section>
 </div><!-- CONTENT-WRAPPER -->
 <?php include_once '../footer.php'; ?>
 </div>
-<script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
-<script src="../bootstrap/js/bootstrap.min.js"></script>
-<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
-<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<script src="../plugins/fastclick/fastclick.js"></script>
-<script src="../dist/js/app.min.js"></script>
 <script src="../dist/js/demo.js"></script>
+<script src="../plugins/jQuery/jQuery-2.1.4.min.js" type="text/javascript"></script>
+<script src="../plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="../dist/js/demo.js" type="text/javascript"></script>
+<script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../plugins/slimScroll/jquery.slimScroll.min.js" type="text/javascript"></script>
+<script src='../plugins/fastclick/fastclick.min.js'></script>
+<script src="../plugins/datatables/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="../plugins/datatables/dataTables.bootstrap.min.js" type="text/javascript"></script>
+<script src="../dist/js/app.min.js"></script>
 <script>
   $(function () {
-    $('#laudos').DataTable({
-      "paging": true,
-      "lengthChange": true,
-      "searching": true,
-      "ordering": false,
-      "info": true,
-      "autoWidth": false
-    });
+    $("#laudo").DataTable();
   });
 </script>
-
-
-
 <script language="JavaScript">
 function abrir(URL) {
  
